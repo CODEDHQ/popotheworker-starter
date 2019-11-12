bea405f07698fc11702c6721eea08c895e1b49f3
Mshary <themshary@Msharys-MacBook-Pro.local>
2019-11-12 15:47:49 +0300

    Chapter 8

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,15 +1,67 @@
 import React, { Component } from "react";
 import tasksStore from "../Stores/TasksStore";
 import { observer } from "mobx-react";
-import { MDBListGroupItem } from "mdbreact";
+import {
+  MDBListGroupItem,
+  MDBModal,
+  MDBModalHeader,
+  MDBModalBody,
+  MDBIcon,
+  MDBModalFooter,
+  MDBBtn,
+  MDBCloseIcon
+} from "mdbreact";
 
 class Task extends Component {
+  state = {
+    modal: false
+  };
+  deleteTask = () => {
+    this.toggleDeleteModal();
+    console.log(this.props.task.id);
+    tasksStore.deleteTask(this.props.task.id);
+  };
+  toggleDeleteModal = () => {
+    this.setState({ modal: !this.state.modal });
+  };
   render() {
     let dueDate;
     if (this.props.task.due)
       dueDate = <small>{this.props.task.due.fromNow()}</small>;
     return (
       <MDBListGroupItem>
+        <MDBModal
+          modalStyle="danger"
+          className="text-white"
+          size="sm"
+          position="top"
+          isOpen={this.state.modal}
+          toggle={this.toggleDeleteModal.bind(this)}
+        >
+          <MDBModalHeader
+            className="text-center"
+            titleClass="w-100"
+            tag="p"
+            toggle={this.toggleDeleteModal.bind(this)}
+          >
+            Are you sure?
+          </MDBModalHeader>
+          <MDBModalBody className="text-center">
+            <MDBIcon icon="times" size="4x" pulse className="animated tada" />
+          </MDBModalBody>
+          <MDBModalFooter className="justify-content-center">
+            <MDBBtn color="danger" onClick={this.deleteTask.bind(this)}>
+              Yes
+            </MDBBtn>
+            <MDBBtn
+              color="danger"
+              outline
+              onClick={this.toggleDeleteModal.bind(this)}
+            >
+              No
+            </MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
         <div className="d-flex justify-content-between">
           <div className="d-flex align-items-start flex-column">
             <div className="d-flex justify-content-start">
@@ -17,6 +69,12 @@ class Task extends Component {
                 <h5 className="mb-1">{this.props.task.title}</h5>
               </div>
             </div>
+          </div>
+          <div>
+            <MDBCloseIcon
+              className="ml-auto"
+              onClick={this.toggleDeleteModal}
+            />
           </div>
         </div>
         <p className="mb-1">{this.props.task.details}</p>
--- src/Stores/TasksStore.js
+++ src/Stores/TasksStore.js
@@ -2,16 +2,19 @@ import { decorate, observable } from "mobx";
 import moment from "moment";
 
 class TasksStore {
+  idCounter = 3;
   todayTasks = [
     {
       title: "Eat a banana",
       details: "Find a banana. Eat it.",
-      due: moment()
+      due: moment(),
+      id: 1
     },
     {
       title: "Tell The Monkey to get off his monkey butt and do something.",
       details: "",
-      due: moment()
+      due: moment(),
+      id: 2
     }
   ];
   futureTasks = [];
@@ -19,7 +22,8 @@ class TasksStore {
     // This next line will stringify the tasks list
     let tasks = JSON.stringify({
       todayTasks: this.todayTasks,
-      futureTasks: this.futureTasks
+      futureTasks: this.futureTasks,
+      idCounter: this.idCounter
     });
     localStorage.setItem("tasks", tasks);
   };
@@ -35,10 +39,17 @@ class TasksStore {
       });
       this.todayTasks = tasks.todayTasks;
       this.futureTasks = tasks.futureTasks;
+      this.idCounter = tasks.idCounter;
     }
   };
   addTask = (title, details, due) => {
-    let newTask = { title: title, details: details, due: due };
+    let newTask = {
+      title: title,
+      details: details,
+      due: due,
+      id: this.idCounter
+    };
+    this.idCounter++;
     if (due && due.isAfter(moment(), "day")) {
       let tasks = this.futureTasks;
       tasks.push(newTask);
@@ -48,6 +59,14 @@ class TasksStore {
       tasks.push(newTask);
       this.todayTasks = tasks;
     }
+    this.updateLocalStorage();
+  };
+  deleteTask = taskId => {
+    // Remove task from today and future tasks.
+    this.todayTasks = this.todayTasks.filter(item => item.id !== taskId);
+    this.futureTasks = this.futureTasks.filter(item => item.id !== taskId);
+
+    // Update local storage.
     this.updateLocalStorage();
   };
 }
